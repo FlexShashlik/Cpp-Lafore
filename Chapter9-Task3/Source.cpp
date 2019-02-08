2c0d469747836f9a9de1850096dcc5e553626008
Sevada Saakyan <sevada992@gmail.com>
2019-02-08 03:32:02 +0300

    Chapter 9 was added.

--- Chapter9-Task3/Source.cpp
+++ Chapter9-Task3/Source.cpp
@@ -26,7 +26,31 @@ public:
 	}
 };
 
-class book : private publication
+class sales
+{
+private:
+	enum { MONTH = 3 };
+	float salesArr[MONTH];
+public:
+	void putdata()const
+	{
+		for (int i = 0; i < MONTH; i++)
+		{
+			cout << "\nProfit for " << i + 1 << " month - " << salesArr[i];
+		}
+	}
+
+	void getdata()
+	{
+		for (int i = 0; i < MONTH; i++)
+		{
+			cout << "Enter profit for " << i + 1 << " month: ";
+			cin >> salesArr[i];
+		}
+	}
+};
+
+class book : private publication, private sales
 {
 private:
 	int pages;
@@ -35,6 +59,7 @@ public:
 	{
 		publication::putdata();
 		cout << "\nPages: " << pages;
+		sales::putdata();
 	}
 
 	void getdata()
@@ -42,10 +67,11 @@ public:
 		publication::getdata();
 		cout << "Enter pages: ";
 		cin >> pages;
+		sales::getdata();
 	}
 };
 
-class tape : private publication
+class tape : private publication, private sales
 {
 private:
 	float duration;
@@ -54,6 +80,7 @@ public:
 	{
 		publication::putdata();
 		cout << "\nDuration: " << duration;
+		sales::putdata();
 	}
 
 	void getdata()
@@ -61,24 +88,18 @@ public:
 		publication::getdata();
 		cout << "Enter duration: ";
 		cin >> duration;
+		sales::getdata();
 	}
 };
 
-int main() 
+int main()
 {
-	publication p1;
-	p1.putdata();
-	p1.getdata();
-	p1.putdata();
+	book b;
+	tape t;
 
-	book b1, b2;
-	b1.getdata();
-	b2.getdata();
+	t.getdata();
+	b.getdata();
 
-	tape t1;
-	t1.getdata();
-
-	b1.putdata();
-	b2.putdata();
-	t1.putdata();
+	t.putdata();
+	b.putdata();
 }
